d90e7498fcbd86705bb23e1fb1756cae0cfd7fe6
Necnion8 <necnionch@gmail.com>
2023-02-05 18:51:03 +0900

    [Add]Tools.parseCommandArguments() method

--- SMFB_Core/src/main/java/com/github/nova_27/mcplugin/servermanager/core/utils/Tools.java
+++ SMFB_Core/src/main/java/com/github/nova_27/mcplugin/servermanager/core/utils/Tools.java
@@ -1,5 +1,8 @@
 package com.github.nova_27.mcplugin.servermanager.core.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * ツール
  */
@@ -18,4 +21,45 @@ public class Tools {
         }
         return original;
     }
+
+    /**
+     * https://gist.github.com/raymyers/8077031
+     * add custom fix
+     */
+    public static List<String> parseCommandArguments(CharSequence string) {
+        List<String> tokens = new ArrayList<>();
+        boolean escaping = false;
+        char quoteChar = ' ';
+        boolean quoting = false;
+        int lastCloseQuoteIndex = Integer.MIN_VALUE;
+        StringBuilder current = new StringBuilder();
+        for (int i = 0; i<string.length(); i++) {
+            char c = string.charAt(i);
+            if (escaping) {
+                current.append(c);
+                escaping = false;
+            } else if (c == '\\' && (!quoting && (quoteChar == '\'' || quoteChar == '"'))) {
+                escaping = true;
+            } else if (quoting && c == quoteChar) {
+                quoting = false;
+                lastCloseQuoteIndex = i;
+            } else if (!quoting && (c == '\'' || c == '"')) {
+                quoting = true;
+                quoteChar = c;
+            } else if (!quoting && Character.isWhitespace(c)) {
+                if (current.length() > 0 || lastCloseQuoteIndex == (i - 1)) {
+                    tokens.add(current.toString());
+                    current = new StringBuilder();
+                }
+            } else {
+                current.append(c);
+            }
+        }
+        if (current.length() > 0 || lastCloseQuoteIndex == (string.length() - 1)) {
+            tokens.add(current.toString());
+        }
+
+        return tokens;
+    }
+
 }
